566fcd884dc8834c8efbe8881c81baba14dd6f86
iraklislivin <islivin@vaba.co>
2023-01-24 21:30:16 +0400

    Tech Debt

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -67,7 +67,6 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
