a48e4019bf2e4b5598aa962178a21f8d30c48410
iraklislivin <islivin@vaba.co>
2023-01-24 22:48:44 +0400

    Tech Debt

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,149 @@
 class Article < ApplicationRecord
   validates :name, length: {maximum: 10}
+
+  def bad_code
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+          if name == 'asdsds'
+            return 'asdsds'
+          else
+            return 'asdsdsds'
+          end
+        end
+      end
+    end
+
+  end
+  def bad_code4
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
+
+  def bad_code3
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
+
+  def bad_code2
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
+
+  def bad_code6
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
+
+  def bad_code7
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
+
+  def bad_code8
+    if 2 == 1
+      return 'Nope'
+    else
+      if name == 'test'
+        return 'No good'
+      else
+        if name == 'test2'
+          return ':dsdsdsds'
+        elsif
+        if name == 'asdsds'
+          return 'asdsds'
+        else
+          return 'asdsdsds'
+        end
+        end
+      end
+    end
+
+  end
 end
